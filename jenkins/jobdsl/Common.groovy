4daf790e27e08efa88dba87ac74d7c422e8d33a7
himatej <himatej@beehyv.com>
2018-05-24 11:32:49 +0530

    changed project url

--- jenkins/jobdsl/Common.groovy
+++ jenkins/jobdsl/Common.groovy
@@ -1,6 +1,6 @@
 class Common{
     final static githubUser = 'himatej'
     final static githubURL = 'himatej/test'
-    final static githubProjectURL = 'himatej/test'
+    final static githubProjectURL = 'https://github.com/himatej/test'
     final static githubCredentialsID = '58dce1a2-9b50-4f78-b828-cb24fa8f1fe3'
 }
