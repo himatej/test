673e35510040f3af37534fcd16d7f0a4cb428762
himatej <himatej@beehyv.com>
2018-05-25 19:54:53 +0530

    added scm

--- jenkins/jobdsl/seed.groovy
+++ jenkins/jobdsl/seed.groovy
@@ -1,4 +1,16 @@
+import Common
 freeStyleJob(binding.variables.get('binding_branch')+'/seed'){
+    scm {
+        git {
+            remote {
+                credentials(Common.githubCredentialsID)
+                github(Common.githubURL)
+            }
+
+            //TODO parameterize branch name
+            branch('refs/remotes/origin/'+binding.variables.get('binding_branch'))
+        }
+    }
     steps{
         dsl{
             lookupStrategy('SEED_JOB')
--- jenkins/jobdsl/seed_subfolder.groovy
+++ jenkins/jobdsl/seed_subfolder.groovy
@@ -7,8 +7,21 @@ freeStyleJob('seed_subfolder'){
         env('binding_branch', '${BRANCH_TO_GENERATE}')
     }
 
+    scm {
+        git {
+            remote {
+                credentials('93c6a674-4dbd-47de-b9af-aabf9462c183')
+                github('himatej/test')
+            }
+
+            //TODO parameterize branch name
+            branch('refs/remotes/origin/${BRANCH_TO_GENERATE}')
+        }
+    }
+
     steps{
         dsl{
+            lookupStrategy('SEED_JOB')
             text("folder(binding.variables.get('binding_branch'))")
         }
         dsl{
@@ -17,7 +30,13 @@ freeStyleJob('seed_subfolder'){
         }
 
         downstreamParameterized{
-            trigger('${BRANCH_TO_BUILD}/seed')
+            trigger('${BRANCH_TO_GENERATE}/seed'){
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+            }
         }
     }
 }
