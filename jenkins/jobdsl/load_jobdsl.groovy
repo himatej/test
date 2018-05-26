daa5c4a0024e1cec3c3a86553eab94c5388186b7
himatej <himatej@beehyv.com>
2018-05-26 15:11:48 +0530

    added condition for not generating seed_subfolder in master

--- jenkins/jobdsl/load_jobdsl.groovy
+++ jenkins/jobdsl/load_jobdsl.groovy
@@ -1,4 +1,8 @@
 new ControllerJob(binding).generate()
 new DatapathJob(binding).generate()
 new GhPrbHook(binding).generate()
-new SeedSubFolderJob(binding).generate()
+
+//do not generate seed subfolder for master
+if(!binding.variables.get('BUILD_URL').contains('master')){
+    new SeedSubFolderJob(binding).generate()
+}
